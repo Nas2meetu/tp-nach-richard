2c1cb58a161dba1a07e3a0237fafe3213dccb21b
reugui@gmail.com <reugui@gmail.com@5e5f040f-4371-08a4-6efd-05ee6025190b>
2012-11-23 08:10:59 +0000

    subida la practica 2 sin empezar

---
+++ P2/src/tp/pr2/Action.java
@@ -0,0 +1,22 @@
+package tp.pr1;
+
+/**
+*
+* @author Ignacio Cerda Sanchez
+* @author Ricardo Eugui Fernandez
+* @version 1
+*
+*/
+
+
+public enum Action {
+	
+	 /**
+	  *  Define valid instructions for Wall·E
+	  */
+
+	MOVE, TURN, HELP, QUIT, UNKNOWN 
+		
+}
+	
+
---
+++ P2/src/tp/pr2/Place.java
@@ -0,0 +1,55 @@
+package tp.pr1;
+import static tp.pr1.Constants.LINE_SEPARATOR;
+
+/**
+*
+* @author Ignacio Cerda Sanchez
+* @author Ricardo Eugui Fernandez
+* @version 1
+* 
+*/
+
+
+public class Place {
+	
+
+	private String placeName;
+	private boolean isSpaceship;
+	private String placeDescription;
+	
+	/**
+	 * Constructor of three parameters with name and description place and if this place has a Robot Space Ship
+	 * @param placeName is place name
+	 * @param isSpaceship is place where Spaceship is it
+	 * @param placeDescription is information about place
+	 * 
+	 */
+
+	public Place(String placeName, boolean isSpaceship, String placeDescription) {
+		this.placeName = placeName;
+		this.isSpaceship = isSpaceship;
+		this.placeDescription = placeDescription;
+	}
+	
+	/**
+	 * 
+	 * @return isSpaceship
+	 * 
+	 */
+
+	public boolean isSpaceship() {
+		return isSpaceship;
+	}
+
+	
+	/**
+	 * Override toString to show place information
+	 */
+	
+	public String toString() {
+		return  placeName + LINE_SEPARATOR + placeDescription;
+	}
+	
+	
+
+}
