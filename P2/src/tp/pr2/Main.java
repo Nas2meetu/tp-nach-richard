d99be95aab2b9bc59b24e538c198bc0a792092ad
reugui <reugui@5e5f040f-4371-08a4-6efd-05ee6025190b>
2012-12-13 19:22:54 +0000

    actualizado el mapa con los items

--- P2/src/tp/pr2/Main.java
+++ P2/src/tp/pr2/Main.java
@@ -18,59 +18,59 @@ public class Main {
 	
 	private static Place[] createPlaces(){
 		Place [] _places = new Place[10];
-		// 0: Puerta del Sol
-		_places[0] = new Place("PUERTA DEL SOL" , false, 
-				"You are at the PUERTA DEL SOL, the center of Madrid. " +  LINE_SEPARATOR +
-				"Ufff, there are a lot of streets, but all of them are full of garbage." + LINE_SEPARATOR +
-				"You should walk around this place to look for some interesting" + LINE_SEPARATOR +
-				"objects to pick up." + LINE_SEPARATOR +
-				"Note that there is a big clock. Remember, leave the square before " + LINE_SEPARATOR +
-				"night. It can be dangerous");
-		// 1: Plaza Mayor
-		_places[1] = new Place("PLAZA MAYOR", false,
-			"Mmmh... it smells squid fried in butter. You should try to eat something");
+		 // 0: Puerta del Sol
+        _places[0] = new Place("PUERTA DEL SOL" , false,
+                "You are at the PUERTA DEL SOL, the center of Madrid. " +  LINE_SEPARATOR +
+                "Ufff, there are a lot of streets, but all of them are full of garbage." + LINE_SEPARATOR +
+                "You should walk around this place to look for some interesting" + LINE_SEPARATOR +
+                "objects to pick up." + LINE_SEPARATOR +
+                "Note that there is a big clock. Remember, leave the square before " + LINE_SEPARATOR +
+                "night. It can be dangerous");
+        // 1: Plaza Mayor
+        _places[1] = new Place("PLAZA MAYOR", false,
+            "Mmmh... it smells squid fried in butter. You should try to eat something");
 
-		// 2: Plaza Espa�a
-		_places[2] = new Place("PLAZA ESPA�A" , false,
-			"What a big square! You have a big park where you can sleep under a tree." + LINE_SEPARATOR +
-			"But you have a problem, you have to come back to PLAZA MAYOR. " + LINE_SEPARATOR +
-			"There is no other exit");
+        // 2: Plaza EspaÃ±a
+        _places[2] = new Place("PLAZA ESPAÃ‘A" , false,
+            "What a big square! You have a big park where you can sleep under a tree." + LINE_SEPARATOR +
+            "But you have a problem, you have to come back to PLAZA MAYOR. " + LINE_SEPARATOR +
+            "There is no other exit");
 
-		// 3: Plaza de Callao 
-		_places[3] = new Place("CALLAO", false, 
-			"In this small square you can find some some fuel. " + LINE_SEPARATOR +
-			"Go to fnac and take the fuel for the heating");
-		
-		// 4: Plaza de Col�n (END)
-		_places[4] = new Place("COLON", true, 
-			"Sometime ago, Spanish people concentrates here to watch football " + LINE_SEPARATOR +
-			"in a big screen." +  LINE_SEPARATOR +
-			"Wall-e, did you know that in Spain there were very good football teams?." +  LINE_SEPARATOR +
-			"Look for cans! People throw cans after watching football!");
-		
-		// 5: Cibeles
-		_places[5] = new Place("CIBELES", false, 
-			"Arggg, the fountain is ugly! The water is very dirty. " + LINE_SEPARATOR + 
-			"You should leave before the lions attack you");
-		
-		// 6: Neptuno
-		_places[6] = new Place("NEPTUNO", false,
-			"Watch Wall-e! Another fountain! Perhaps this one has water for drinking" + LINE_SEPARATOR + 
-			"If you are cold, use that red and white scarf");
-		
-		// 7: Atocha 
-		_places[7] = new Place("ATOCHA", false,
-			"You have a lot of old trains here, but they do not work" +  LINE_SEPARATOR + 
-			"All trains were destroyed during the crisis of 2013" + LINE_SEPARATOR + 
-			"Take all the iron you find");
-		
-		// 8: Puerta de Alcala
-		_places[8] = new Place("PUERTA DE ALCALA", true,
-			"Ok, finally you have found your spaceship....");
-		
-		// 9: Plaza de Jacinto Benavente
-		_places[9] = new Place("JACINTO BENAVENTE",false,
-			"If you are cold you can start a fire with the wheels of those old buses");
+        // 3: Plaza de Callao
+        _places[3] = new Place("CALLAO", false,
+            "In this small square you can find some some fuel. " + LINE_SEPARATOR +
+            "Go to fnac and take the fuel for the heating");
+       
+        // 4: Plaza de ColÃ³n (END)
+        _places[4] = new Place("COLON", true,
+            "Sometime ago, Spanish people concentrates here to watch football " + LINE_SEPARATOR +
+            "in a big screen." +  LINE_SEPARATOR +
+            "Wall-e, did you know that in Spain there were very good football teams?." +  LINE_SEPARATOR +
+            "Look for cans! People throw cans after watching football!");
+       
+        // 5: Cibeles
+        _places[5] = new Place("CIBELES", false,
+            "Arggg, the fountain is ugly! The water is very dirty. " + LINE_SEPARATOR +
+            "You should leave before the lions attack you");
+       
+        // 6: Neptuno
+        _places[6] = new Place("NEPTUNO", false,
+            "Watch Wall-e! Another fountain! Perhaps this one has water for drinking" + LINE_SEPARATOR +
+            "If you are cold, use that red and white scarf");
+       
+        // 7: Atocha
+        _places[7] = new Place("ATOCHA", false,
+            "You have a lot of old trains here, but they do not work" +  LINE_SEPARATOR +
+            "All trains were destroyed during the crisis of 2013" + LINE_SEPARATOR +
+            "Take all the iron you find");
+       
+        // 8: Puerta de Alcala
+        _places[8] = new Place("PUERTA DE ALCALA", true,
+            "Ok, finally you have found your spaceship....");
+       
+        // 9: Plaza de Jacinto Benavente
+        _places[9] = new Place("JACINTO BENAVENTE",false,
+            "If you are cold you can start a fire with the wheels of those old buses");
 
 		  return _places;
 	}
@@ -101,7 +101,34 @@ public class Main {
 		streets.add(new Street(_places[6],Direction.SOUTH, _places[7]));
 		streets.add(new Street(_places[9],Direction.EAST, _places[6]));
 		
-		Street [] s = new Street[0];
+		_places[0].addItem(new Garbage("newspapers","news on sports",5));
+        _places[0].addItem(new Fuel("grapes","celebrations of the new year",1,1));
+        _places[0].addItem(new CodeCard("Spaceballs-card","This is the kind of combination an idiot would have on his luggage","12345"));
+        _places[0].addItem(new Fuel("Coal","Be careful with this fuel because it is extremely contaminant",-80,1));
+        
+        _places[1].addItem(new Fuel("gas-oil","from the buses..",5,2));
+        _places[1].addItem(new Garbage("explosive-plastic","too dangerous to be in the street",10));
+        _places[1].addItem(new CodeCard("matrix-card","It shows you how deep the rabbit hole goes","red_pill"));
+        
+        _places[3].addItem(new Garbage("cans-of-beer","all of them are empty",2));
+        _places[3].addItem(new Fuel("petrol","from olds heatings",10,3));
+        
+        _places[4].addItem(new Fuel("battery","to get cracking",4,1));
+        _places[4].addItem(new Garbage("television","it is broken. Better. Usually programs are really bad",10));
+        
+        _places[5].addItem(new Fuel("water","to put out possible fires",11,3));
+        _places[5].addItem(new Garbage("white-scarf","good for winter",2));
+        _places[5].addItem(new CodeCard("walle-card","I lost it when I was looking for garbage","eva"));
+        _places[5].addItem(new CodeCard("Spaceballs-card","This is the kind of combination an idiot would have on his luggage","12345"));
+        
+        _places[6].addItem(new Garbage("tinned-food","it seems spanish people were hungry",11));
+        _places[6].addItem(new Garbage("yellow-press","what stupid things they write",1));
+        
+        _places[7].addItem(new Fuel("red-bull","good for flying",5,1));
+        _places[7].addItem(new CodeCard("falken-card","Shall we play a game?","Joshua"));
+		
+		
+        Street [] s = new Street[0];
 		s = streets.toArray(s);
 		return s;
 	}
@@ -116,8 +143,9 @@ public class Main {
 		Street[] streets = createStreets(places);
 		
 		
-		RobotEngine engine = new RobotEngine(new City(streets), places[0], Direction.NORTH); 
+		RobotEngine engine = new RobotEngine(new City(streets), 
+								places[0], Direction.NORTH); 
 		engine.startEngine();
 	}
-
+	
 }
