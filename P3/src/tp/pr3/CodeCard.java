5402977cdaa43508bb912af5dc8007090dcc09bc
reugui@gmail.com <reugui@gmail.com@5e5f040f-4371-08a4-6efd-05ee6025190b>
2013-01-03 14:06:12 +0000

    ahora cierra puertas, antes no

--- P3/src/tp/pr3/CodeCard.java
+++ P3/src/tp/pr3/CodeCard.java
@@ -25,15 +25,18 @@ public class CodeCard extends Item {
 	}
 
 	public boolean use(RobotEngine robot, Place where) {
-		if ((this.code == null) || (where == null)){
-			return false;
-		}if (robot.getHeadingStreet() != null && this.code.equals(robot.getHeadingStreet().getCode())){
-			robot.getHeadingStreet().open(this);
-			return true;
-		}else
-			return false;
-			
-		}
+        if ((this.code == null) || (where == null)){
+            return false;
+        }if (robot.getHeadingStreet() != null && this.code.equals(robot.getHeadingStreet().getCode())){
+            if (robot.getHeadingStreet().isOpen())
+                robot.getHeadingStreet().close(this);
+            else
+                robot.getHeadingStreet().open(this);
+            return true;
+        }else
+            return false;
+           
+    }
 
 	public String getCode() {
 		return code;
