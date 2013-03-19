0fc58c9ac3172cd75a4b4547fc93d60f275db1c0
nacho85 <nacho85@5e5f040f-4371-08a4-6efd-05ee6025190b>
2013-03-19 16:50:13 +0000

    Añadidas las nuevas clases de la práctica 4 vacias

---
+++ P3/src/tp/pr4/gui/MainWindow.java
@@ -0,0 +1,11 @@
+package tp.pr4.gui;
+
+import tp.pr4.RobotEngine;
+
+public class MainWindow {
+
+	public MainWindow(RobotEngine robot){
+		
+	}
+	
+}
---
+++ P3/src/tp/pr4/gui/NavigationPanel.java
@@ -0,0 +1,16 @@
+package tp.pr4.gui;
+
+/**
+*
+* @author Ignacio Cerda Sanchez
+* @author Ricardo Eugui Fernandez
+* @version 4
+*
+*/
+
+public class NavigationPanel extends javax.swing.JPanel {
+
+	public NavigationPanel() {
+		
+	}
+}
