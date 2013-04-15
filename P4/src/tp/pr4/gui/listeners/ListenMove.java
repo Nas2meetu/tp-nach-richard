2a1d2953a28c516d0b783f328c8aab27db3d06d0
nacho85 <nacho85@5e5f040f-4371-08a4-6efd-05ee6025190b>
2013-04-15 11:48:14 +0000

    Realizada la operacion Turn

---
+++ P4/src/tp/pr4/gui/listeners/ListenMove.java
@@ -0,0 +1,23 @@
+package tp.pr4.gui.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import tp.pr4.City;
+import tp.pr4.gui.InstructionPanel;
+import tp.pr4.gui.NavigationPanel;
+
+public class ListenMove implements ActionListener {
+
+	public ListenMove(NavigationPanel navPanel, City city,
+			InstructionPanel instructionPanel) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
