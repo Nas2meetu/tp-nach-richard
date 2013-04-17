1554b3eeb6bcd5d6825a578bb6b810d0728db1bc
nacho85 <nacho85@5e5f040f-4371-08a4-6efd-05ee6025190b>
2013-04-17 17:35:46 +0000

    Implementado commons.cli en Main y funcionando.

--- P4/src/tp/pr4/Main.java
+++ P4/src/tp/pr4/Main.java
@@ -1,13 +1,15 @@
 package tp.pr4;
 
-import static tp.pr4.Constants.*;
-
 import java.awt.EventQueue;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 
@@ -30,7 +32,7 @@ public class Main {
 	 */
 	
 	
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		if (args.length == 0) {
 			System.err.println(BAD_PARAMS);
 			System.err.println(USAGE_MAPFILE + LINE_SEPARATOR);
@@ -54,7 +56,7 @@ public class Main {
 				System.exit(2);
 			}
 		}
-	}
+	}*/
 	
 	/*public static void main(String[] args) {
 		
@@ -77,4 +79,85 @@ public class Main {
 		
 		
 	}*/
+	public static void main(String[] args) {
+
+		CityLoaderFromTxtFile fileLoader = null;
+		City city = null;
+		CommandLineParser cmdParser = null;
+		CommandLine cmdLine = null;
+
+		// Options parser
+
+		Options options = new Options();
+		Option helpOption = new Option("h", "help", false,
+				"Show this help message");
+		Option interfacesOption = new Option("i", "interface", true,
+				"The type of interface: console or swing");
+		Option mapsOption = new Option("m", "map", true,
+				"File with the description of the city");
+		mapsOption.setArgName("mapfile");
+
+		options.addOption(helpOption);
+		options.addOption(interfacesOption);
+		options.addOption(mapsOption);
+
+		try {
+			cmdParser = new BasicParser();
+			cmdLine = cmdParser.parse(options, args);
+
+			if (cmdLine.hasOption("h")) {
+				new HelpFormatter().printHelp(Main.class.getCanonicalName(),
+						options);
+			}
+
+			else {
+				String map = cmdLine.getOptionValue("m");
+				if (map != null) {
+					try {
+						InputStream file = new FileInputStream(map);
+						fileLoader = new CityLoaderFromTxtFile();
+						city = fileLoader.loadCity(file); //madrid.txt
+					} catch (FileNotFoundException e) {
+						System.err.println(args[0]);
+						System.exit(2);
+					} catch (IOException e) {
+						System.err.println();
+						System.exit(2);
+					}
+				} else {
+					System.out.println("Introduce a map please.");
+				}
+
+				String interfaces = cmdLine.getOptionValue("i");
+				if (interfaces != null) {
+
+					if (interfaces.equalsIgnoreCase("swing")) {
+						RobotEngine robot = new RobotEngine(city,
+								fileLoader.getInitialPlace(), Direction.NORTH);
+						final MainWindow gameWindow = new MainWindow(robot);
+						robot.startEngine();
+						EventQueue.invokeLater(new Runnable() {
+							public void run() {
+								gameWindow.pack();
+								gameWindow.setLocationRelativeTo(null);
+								gameWindow.setVisible(true);
+							}
+						});
+					} else if (interfaces.equalsIgnoreCase("console")) {
+						RobotEngine robot = new RobotEngine(city,
+								fileLoader.getInitialPlace(), Direction.NORTH);
+						robot.startEngine();
+					} else {
+						System.out.println("Console or Swing?");
+					}
+
+				} else {
+					System.out.println("Introduce an interface please.");
+				}
+			}
+
+		} catch (org.apache.commons.cli.ParseException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
